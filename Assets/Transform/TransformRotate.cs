d4d58b3b0baeae90f300daf70813ad63169d1c79
Мусин Михаил <musin.mihail@gmail.com>
2021-08-01 23:08:52 +0300

    Transform Rotate

---
+++ Assets/Transform/TransformRotate.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class TransformRotate : MonoBehaviour
+{
+    // private GameObject cube1;
+    // Vector3 axis = new Vector3(235,0,235);
+    // void Awake()
+    // {
+    //     cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    //     cube1.transform.position = Vector3.zero;
+    //     cube1.GetComponent<Renderer>().material.color = Color.red;
+    //     cube1.name = "Self";
+    // }
+    // void Update()
+    // {
+    //     cube1.transform.Rotate(axis, 0.3f, Space.World);
+    // }
+
+    // private GameObject cube1;
+    // Vector3 eulers = new Vector3(0,0,0.1f);
+    // void Awake()
+    // {
+    //     cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    //     cube1.transform.position = Vector3.zero;
+    //     cube1.GetComponent<Renderer>().material.color = Color.red;
+    //     cube1.name = "Self";
+    // }
+    // void Update()
+    // {
+    //     cube1.transform.Rotate(eulers);
+    // }
+
+    // private GameObject cube1, cube2;
+    // void Awake()
+    // {
+    //     cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    //     cube1.transform.position = new Vector3(0.75f, 0.0f, 0.0f);
+    //     cube1.transform.Rotate(90.0f, 0.0f, 0.0f, Space.Self);
+    //     cube1.GetComponent<Renderer>().material.color = Color.red;
+    //     cube1.name = "Self";
+
+    //     cube2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    //     cube2.transform.position = new Vector3(-0.75f, 0.0f, 0.0f);
+    //     cube2.transform.Rotate(90.0f, 0.0f, 0.0f, Space.World);
+    //     cube2.GetComponent<Renderer>().material.color = Color.green;
+    //     cube2.name = "World";
+    // }
+    // void Update()
+    // {
+    //     cube1.transform.Rotate(0, 0, 0.1f, Space.Self);
+    //     cube2.transform.Rotate(0, 0, 0.1f, Space.World);
+    // }
+}
