a027f820099315926b519ada86ef4b16a60074be
Мусин Михаил <musin.mihail@gmail.com>
2021-08-07 18:23:05 +0300

    TransformSetParent

---
+++ Assets/Transform/TransformSetParent.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+public class TransformSetParent : MonoBehaviour
+{
+    // public GameObject child;
+    // public Transform parent;
+    void Update()
+    {
+        // if(Input.GetKeyDown(KeyCode.Alpha1))
+        // {
+        //     child.transform.SetParent(parent);
+        // }
+        // if(Input.GetKeyDown(KeyCode.Alpha2))
+        // {
+        //     child.transform.SetParent(parent, false);
+        //     Debug.Log("transform.position: " + child.transform.position);
+        //     Debug.Log("transform.rotation: " + child.transform.rotation);
+        // }
+        // if(Input.GetKeyDown(KeyCode.Alpha3))
+        // {
+        //     child.transform.SetParent(parent, true);
+        //     Debug.Log("transform.position: " + child.transform.position);
+        //     Debug.Log("transform.rotation: " + child.transform.rotation);
+        // }
+        // if(Input.GetKeyDown(KeyCode.Alpha4))
+        // {
+        //     child.transform.SetParent(null);
+        // }
+    }
+}
