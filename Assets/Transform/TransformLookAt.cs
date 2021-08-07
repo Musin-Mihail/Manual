e86c34aa4106ef5104d8bbae642427d2adf0405b
Мусин Михаил <musin.mihail@gmail.com>
2021-08-07 17:42:30 +0300

    TransformLookAt

---
+++ Assets/Transform/TransformLookAt.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+public class TransformLookAt : MonoBehaviour
+{
+    // public GameObject target;
+    // public Vector3 vector3;
+    void Update()
+    {
+        // transform.LookAt(target.transform);
+        // target.transform.position = vector3;
+        // transform.LookAt(vector3);
+        // if(Input.GetKeyDown(KeyCode.Alpha1))
+        // {
+        //     transform.LookAt(target.transform, Vector3.forward);
+        //     Debug.Log("forward");
+        // }
+        // if(Input.GetKeyDown(KeyCode.Alpha2))
+        // {
+        //     transform.LookAt(target.transform, Vector3.back);
+        //     Debug.Log("back");
+        // }
+        // if(Input.GetKeyDown(KeyCode.Alpha3))
+        // {
+        //     transform.LookAt(target.transform, Vector3.up);
+        //     Debug.Log("up");
+        // }
+        // if(Input.GetKeyDown(KeyCode.Alpha4))
+        // {
+        //     transform.LookAt(target.transform, Vector3.down);
+        //     Debug.Log("down");
+        // }
+        // if(Input.GetKeyDown(KeyCode.Alpha5))
+        // {
+        //     transform.LookAt(target.transform, Vector3.left);
+        //     Debug.Log("left");
+        // }
+        // if(Input.GetKeyDown(KeyCode.Alpha6))
+        // {
+        //     transform.LookAt(target.transform, Vector3.right);
+        //     Debug.Log("right");
+        // }
+    }
+}
