fcb82a0eb977e9826b3862d874d13e50596b6bde
Мусин Михаил <musin.mihail@gmail.com>
2021-08-07 16:44:40 +0300

    TransformTranslate

---
+++ Assets/Transform/TransformTranslate.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+public class TransformTranslate : MonoBehaviour
+{
+    // float speed = 0.01f;
+    // public GameObject target;
+    // public Vector3 vector3;
+    void Update()
+    {
+        // transform.Translate(0, 0, speed);
+        // transform.Translate(0, 0, speed, Space.World);
+        // transform.Translate(0, 0, 0.01f, target.transform);
+        // transform.Translate(vector3);
+    }
+}
