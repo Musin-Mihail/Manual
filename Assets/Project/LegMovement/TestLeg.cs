5b8be67c395abef469bd399b519c16245c31d29e
Мусин Михаил <musin.mihail@gmail.com>
2022-01-09 17:51:48 +0300

    Spider

--- Assets/Project/LegMovement/TestLeg.cs
+++ Assets/Project/LegMovement/TestLeg.cs
@@ -13,19 +13,52 @@ public class TestLeg : MonoBehaviour
         StartCoroutine(DrawLine());
         StartCoroutine(MoveText());
         StartCoroutine(MovePoint());
+        // StartCoroutine(CoefficientSearch());
+    }
+    IEnumerator CoefficientSearch()
+    {
+        Vector3 start = point[point.Count - 1].position;
+        // Vector3 start2 = point[1].position;
+        for (float x = 1; x <= 10; x++)
+        {
+            start.x = x;
+            point[2].position = start;
+            Vector3 vector = Vector3.Lerp(point[0].position, point[2].position, 0.5f);
+            for (float y = 0; y <= 15; y += 0.05f)
+            {
+                vector.y = y;
+                point[1].position = vector;
+                float distance = Vector3.Distance(point[0].position, point[1].position);
+                if (distance >= 10)
+                {
+                    float bisector = (Mathf.Sqrt(10 * 10 * (10 + 10 + x) * (10 + 10 - x)) / (10 + 10));
+                    print(x + " : " + y + " : " + bisector);
+                    break;
+                }
+                yield return new WaitForSeconds(0.01f);
+            }
+            vector.y = 0;
+            point[1].position = vector;
+            yield return new WaitForSeconds(0.01f);
+        }
+
     }
     IEnumerator MovePoint()
     {
         while (true)
         {
-            //Просходит смещение по Z. При этом меняеться длина отрезков. Отрезки должды быть всегда одного размера.
             yield return new WaitForSeconds(0.01f);
             Vector3 vector = Vector3.Lerp(point[0].position, point[2].position, 0.5f);
             Vector3 vector2 = Vector3.Cross(point[2].position - point[0].position, Vector3.forward).normalized + vector;
             point[1].position = vector;
             point[1].LookAt(vector2, Vector3.forward);
-            float distance = Vector3.Distance(point[0].position, point[2].position);
-            point[1].position += point[1].forward * (10 - (distance / 2));
+            float distanceA = Vector3.Distance(point[0].position, point[2].position);
+            float distanceB = 10;
+            float distanceC = 10;
+            float p = (distanceA + distanceB + distanceC) / 2;
+            float heightToSide = (2 / distanceA) * (Mathf.Sqrt(p * (p - distanceA) * (p - distanceB) * (p - distanceC)));
+            float bisector = (Mathf.Sqrt(distanceB * distanceC * (distanceB + distanceC + distanceA) * (distanceB + distanceC - distanceA)) / (distanceB + distanceC));
+            point[1].position += point[1].forward * heightToSide;
         }
     }
     IEnumerator DrawLine()
